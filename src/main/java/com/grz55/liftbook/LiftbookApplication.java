dcc8c72b2a4cf175ece9e5ee061b95e27989147b
grz55 <grz55@vp.pl>
2022-04-14 00:53:15 +0200

    added spring boot base files to repository

---
+++ src/main/java/com/grz55/liftbook/LiftbookApplication.java
@@ -0,0 +1,13 @@
+package com.grz55.liftbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftbookApplication.class, args);
+	}
+
+}
