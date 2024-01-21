82ff86ca4a749adc677265f32a66be8325f8cc86
grz55 <grz55@vp.pl>
2024-01-21 02:55:56 +0100

    feat: cors for different profiles implemented

--- src/main/java/com/grz55/liftbook/config/CrossOriginConfig.java
+++ src/main/java/com/grz55/liftbook/config/CrossOriginConfig.java
@@ -1,16 +1,32 @@
 package com.grz55.liftbook.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CrossOriginConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    @Profile("prod")
+    @Configuration
+    public static class ProdCorsConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("https://grz55.github.io/liftbook-ui/")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+        }
+    }
+
+    @Profile("!prod")
+    @Configuration
+    public static class DevCorsConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:4200")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+        }
     }
 }
